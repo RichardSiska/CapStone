76ca44eaea63c0cff6acb41d14472e9ccc2e41a9
Richard Siska <rs0600826@otc.edu>
2019-03-01 21:56:22 -0600

    Initial Commit
    This is our default app with giving, groups, and events pages added. There is no styling and no linking to other pages set up. This is our starting point.

---
+++ NorthPointApp/Groups.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NorthPointApp
+{
+    public partial class Groups : ContentPage
+    {
+        public Groups()
+        {
+            InitializeComponent();
+        }
+    }
+}
